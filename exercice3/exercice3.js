27bf1e4ae1332f104941a05ae87ffc7c41426a97
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 15:20:48 +0100

    ex3 condition strictement égal + else

--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,16 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let monDiv = document.getElementById('monDiv');
+monDiv.style.width = '10rem';
+monDiv.style.height = '10rem';
+monDiv.style.backgroundColor = "blue";
 
-if()
-{
+let x = 10;
+let y = 10;
 
+if(x === y) {
+    x = x + 3;
+}
+else {
+    monDiv.style.backgroundColor = "red";
 }
 
-{
-
-}
