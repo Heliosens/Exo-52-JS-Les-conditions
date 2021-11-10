072216840359faee0fc59ad34b01c14f18fc47f8
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 15:10:42 +0100

    ex1 condition

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,8 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let x = 5;
+let y = 10;
 
-if x > y
+if (x > y) {
     document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
-
+}
 
